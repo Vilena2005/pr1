dac1bd0c2d1bd03bcdd07121550250066a73b58c
Вилена <Vilena5002@mail.ru>
2025-10-12 00:23:49 +0700

    Add login_style html css

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -5,11 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
     <title>Pop it MVC</title>
 </head>
 <body>
 <header>
-    <nav>
+    <nav class="header-nav">
         <a href="<?= app()->route->getUrl('/hello') ?>">Главная</a>
         <?php
         if (!app()->auth::check()):
@@ -20,6 +21,7 @@
         else:
             ?>
             <a href="<?= app()->route->getUrl('/logout') ?>">Выход (<?= app()->auth::user()->name ?>)</a>
+
         <?php
         endif;
         ?>
--- views/site/signup.php
+++ views/site/signup.php
@@ -1,8 +1,8 @@
-<h2>Регистрация нового пользователя</h2>
+<h2 class="log-title" >Регистрация</h2>
 <h3><?= $message ?? ''; ?></h3>
-<form method="post">
-    <label>Имя <input type="text" name="name"></label>
-    <label>Логин <input type="text" name="login"></label>
-    <label>Пароль <input type="password" name="password"></label>
+<form method="post" class="log-form">
+    <label><input type="text" name="name" placeholder="Имя"></label>
+    <label><input type="text" name="login" placeholder="Логин"></label>
+    <label><input type="password" name="password" placeholder="Пароль"></label>
     <button>Зарегистрироваться</button>
 </form>
